22f9ce65a405fe23553058512ce25b6d3a98676f
18328594608 <1007697416@qq.com>
2024-01-04 18:05:18 +0800

    Change the time

--- http_server/Server.cpp
+++ http_server/Server.cpp
@@ -58,7 +58,7 @@ int Server::start() {
     auto *go_task = WFTaskFactory::create_go_task("go", [this](){
         while (true) {
             readTickLog();
-            usleep(2000000);
+            usleep(5000000);
         }
     });
     go_task->start();
